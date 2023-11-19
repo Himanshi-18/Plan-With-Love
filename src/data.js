2800f19a4f7ad5a48a426ad94c4540f3fd05b42c
Demo-Git <himanshirawat0001@gmail.com>
2023-11-20 01:32:34 +0530

    new project

---
+++ src/data.js
@@ -0,0 +1,54 @@
+const data = [
+    {
+        id: 1,
+        name: "Agra",
+        info:"Agra is a city in the northern state of Uttar Pradesh, India. The city is famous for being the capital of the Mughal emperors from 1526 to 1658. It is a major tourist destination for its many Mughal -era buildings such as Tāj Mahal, Agra Fort and Fatehpūr Sikrī, all three of which are UNESCO World Heritage Sites.",
+        image: "https://cdn.thecodehelp.in/Agra.jpeg",
+        price: "35,758",
+    },
+    {
+        id: 2,
+        name: "Jaipur",
+        info:"Jaipur, city, capital of Rajasthan state, northwestern India. It is situated in the east-central part of the state, roughly equidistant from Alwar (northeast) and Ajmer (southwest). It is Rajasthan most-populous city.Hawa Mahal (Hall of Winds), Jaipur, Rajasthan, India.",
+        image: "https://cdn.thecodehelp.in/Jaipur.jpeg",
+        price: "82,560",
+    },
+    {
+        id: 3,
+        name: "Goa",
+        info:"Goa (Go Goa) is known for its striking landscape, famous beaches in India, astounding monuments and churches and bustling Goa nightlife. Goa is one of the most visited tourist places in India, especially on weekends, as it is not very far from Mumbai and having Panaji as its capital and Vasco da Gama as the largest city.",
+        image: "https://cdn.thecodehelp.in/Goa.jpeg",
+        price: "82,560",
+    },
+    {
+        id: 4,
+        name: "Varanasi",
+        info:"Varanasi is one of the oldest continuously inhabited cities in the world. It was one of the first major urban settlements in the middle Ganges valley. By the 2nd millennium BCE Varanasi was a seat of Vedic religion and philosophy and was also a commercial and industrial centre famous for its muslin and silk fabrics, perfumes, ivory works, and sculpture.",
+        image: "https://cdn.thecodehelp.in/Varanasi.jpeg",
+        price: "31,095",
+    },
+    {
+        id: 5,
+        name: "Darjeeling",
+        info:"Darjeeling, city, extreme northern West Bengal state, northeastern India. It lies about 305 miles (490 km) north of Kolkata, at an elevation of about 7,000 feet (2,100 metres) above sea level. Darjeeling is situated on a long, narrow mountain ridge of the Sikkim Himalayas that descends abruptly to the bed of the Great Rangit River.",
+        image: "https://cdn.thecodehelp.in/Darjeeling.jpeg",
+        price: "28,609",
+    },
+    {
+        id: 6,
+        name: "Jaisalmer",
+        info:"Jaisalmer(pronunciation), nicknamed 'The Golden city', is a city in the Indian state of Rajasthan, located 575 kilometres (357 mi) west of the state capital Jaipur. It is the administrative headquarters of Jaisalmer District. Before Indian independence, the town served as the capital of the Jaisalmer State, ruled by the Bhati Rajputs.",
+        image: "https://cdn.thecodehelp.in/Jaisalmer.jpeg",
+        price: "68,595",
+    },
+    {
+        id: 7,
+        name: "Kochi",
+        info:"Kochi is a city located in the Ernakulam District in the Indian state of Kerala about 200 km from Trivandrum, the capital of Kerala. For many centuries up to and during the British Raj, the city of Kochi was the seat of the eponymous princely state.",
+        image: "https://cdn.thecodehelp.in/Kochi.jpeg",
+        price: "82,560",
+    },
+    
+];
+
+export default data;
